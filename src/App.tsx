de0092bf5df703d06bcc23572de1d0321b155e90
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-05-20 14:38:31 -0300

    pages3

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Hello World</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
