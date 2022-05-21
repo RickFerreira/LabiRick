3b5e83098b133726ecab5e5edfcf491db6b9b4a0
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-05-21 12:20:10 -0300

    iniciando

---
+++ src/App.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #242840;
+    min-height: 100vh;
+    color: #FFF;
+`;
+
+export const Map = styled.div`
+    width: 480px;
+    height: 480px;
+    background-image: url('https://cdn.discordapp.com/attachments/459871999943114762/977313148656836679/labirinto.png');
+    background-position: left top;
+    background-size: 100%;
+`;
---
+++ src/components/Character/index.tsx
@@ -0,0 +1,19 @@
+import * as C from './styles';
+
+type Props = {
+    x: number;
+    y: number;
+}
+
+export const Character = ({x, y}: Props) => {
+    const size = 24;
+
+    return (
+        <C.Container
+            size={size}
+            left={x * size}    
+            top={y * size}
+        >
+        </C.Container>
+    );
+}
